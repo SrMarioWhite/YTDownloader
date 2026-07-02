2084d13f5bdcfe42f9488c6e55b02cc21a650a4d
SrMarioWhite <samuelmontanezsanchez@gmail.com>
2026-07-02 11:36:38 -0500

    Remove YouTube extractor_args from settings
    Removed extractor_args configuration for YouTube.

--- main.py
+++ main.py
@@ -49,11 +49,6 @@ async def procesar_descarga(request: Request):
         'quiet': True,
         'ffmpeg_location': ruta_script,
         'cookiefile': 'cookies.txt',  # Dejar esto igual, el script ya habrá creado el archivo
-        'extractor_args': {
-            'youtube': {
-                'player_client': ['android', 'web']
-            }
-        },
         'postprocessors': []
     }
     
